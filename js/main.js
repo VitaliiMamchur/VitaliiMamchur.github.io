3293eb7d45bb4821c0ecfc1e9a8cfb0eea08e4e6
Vitalii <vitaliiimamchur@gmail.com>
2016-11-04 04:41:45 +0200

    To

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
+//dropdown languages
 $(document).ready(function(){
   $('div.dropdown>button').click(function(){
     $('ul.dropdown-menu').toggle();
 	})
   })
+
+//slight scroller
+$('a').click(function () {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1500);
+    return false;
+});
+
+var top_edge = 1000;
+  $(document).ready(function() {
+    $(window).scroll(function (){
+      if ($(this).scrollTop() > top_edge) $('div.scrollup>button').fadeIn();
+      else $('div.scrollup>button').fadeOut();
+    });
+    $('div.scrollup>button').click(function(){
+      $('body, html').animate({
+        scrollTop: 0
+      }, 1000);
+    });
+  });
